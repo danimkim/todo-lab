d8443458d5f18d71a164f173a654144fb5235350
danimkim <danimkim.dev@gmail.com>
2026-02-15 23:59:50 +0000

    feat: add initial HTML structure and outline todo creation functionality

---
+++ vanilla-js/app.js
@@ -0,0 +1,11 @@
+// Grab the DOM
+const $input = document.getElementById('todo-input');
+const $button = document.getElementById('add-todo');
+const $list = document.getElementById('todo-list');
+
+// Add a event trigger on the button
+$button.addEventListener('click', () => {
+  // create li element
+  // put text input in li element
+  // append li element to ul element
+});
