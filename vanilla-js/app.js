4f753dc5fd88d1ad547be7adb7da81d697ee08e7
danimkim <danimkim.dev@gmail.com>
2026-02-16 02:49:24 +0000

    feat: add todo deletion functionality

--- vanilla-js/app.js
+++ vanilla-js/app.js
@@ -3,20 +3,39 @@ const $button = document.getElementById('add-todo');
 const $list = document.getElementById('todo-list');
 
 const createTodos = () => {
-  // create li element
   const $todoItem = document.createElement('li');
-  // put text input in li element
-  $todoItem.textContent = $input.value;
-  // append li element to ul element
+  const $textContainer = document.createElement('span');
+
+  // create delete button
+  const $deleteBtn = document.createElement('button');
+  $deleteBtn.textContent = 'Delete';
+
+  // Add event trigger for deleting todo
+  $deleteBtn.addEventListener('click', (e) => {
+    const $targetElem = e.target.parentElement;
+    deleteTodo($targetElem);
+  });
+
+  $todoItem.appendChild($textContainer).textContent = $input.value;
+  $todoItem.appendChild($deleteBtn);
   $list.appendChild($todoItem);
 };
 
+// Delete todo
+const deleteTodo = (targetTodo) => targetTodo.remove();
+
 // When Add todo button is clicked
-$button.addEventListener('click', createTodos);
+$button.addEventListener('click', () => {
+  createTodos();
+  // Reset input
+  $input.value = '';
+});
 
 // When pressing Enter
 $input.addEventListener('keydown', (e) => {
   if (e.key === 'Enter') {
     createTodos();
+    // Reset input
+    $input.value = '';
   }
 });
