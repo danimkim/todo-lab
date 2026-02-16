fae6b10d929953ccfc983c0f35fbfe455fcf1b2a
danimkim <danimkim.dev@gmail.com>
2026-02-16 00:43:38 +0000

    feat: implement todo creation functionality

--- vanilla-js/app.js
+++ vanilla-js/app.js
@@ -1,11 +1,22 @@
-// Grab the DOM
 const $input = document.getElementById('todo-input');
 const $button = document.getElementById('add-todo');
 const $list = document.getElementById('todo-list');
 
-// Add a event trigger on the button
-$button.addEventListener('click', () => {
+const createTodos = () => {
   // create li element
+  const $todoItem = document.createElement('li');
   // put text input in li element
+  $todoItem.textContent = $input.value;
   // append li element to ul element
+  $list.appendChild($todoItem);
+};
+
+// When Add todo button is clicked
+$button.addEventListener('click', createTodos);
+
+// When pressing Enter
+$input.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    createTodos();
+  }
 });
